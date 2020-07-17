a5e2264837cda42c84f8e91fa2b100e3d87ad5d3
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-17 06:26:33 -0700

    commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="plain_db",
-    version="0.0.24",
+    version="0.0.25",
     author="Yunzhi Gao",
     author_email="gaoyunzhi@gmail.com",
     description="Plain txt DB",
